88f4cea92c52fbb92a4520ca29b9ea46b9c8007c
kpate135 <kpate403050@gmail.com>
2022-03-16 23:25:41 -0700

    final fixes and removing comments

--- Custom Project Final Demo/custom_project_final_demo/custom_project_final_demo.ino
+++ Custom Project Final Demo/custom_project_final_demo/custom_project_final_demo.ino
@@ -15,20 +15,6 @@
  *         Demo Link: PUT VIDEO HERE
  */
 
-
-/*
-const int b0 = 2;
-const int b1 = 3;
-const int b2 = 4;
-const int b3 = 5;
-const int b4 = 6;
-const int b5 = 7;
-const int b6 = 8;
-const int b7 = 9;
-int a = 0x00;
-int a0, a1, a2, a3, a4, a5, a6, a7;
-*/
-
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
@@ -45,13 +31,17 @@ int micSensor_intensity = 11; //digital gives LOW or HIGH sound intensity
 
 int ledTimer = 0;
 
+int button1 = A1;
+
 int buzzer = 9;
-bool buzzerEnabled = true; //true for now, later implement menu within nokia 5110 lcd to toggle buzzerEnabled.
+bool buzzerEnabled = true; //toggle buzzerEnabled via button press
 bool threshold_reached = false;
 bool buzzer_threshold = false;
 
 bool intensityValue = false;
 int micLevel = 0;
+
+//string nokiaDecibel; 
 
 typedef struct task {
   int state;
@@ -60,7 +50,7 @@ typedef struct task {
   int (*TickFct)(int);
 } task;
 
-const unsigned short tasksNum = 3;
+const unsigned short tasksNum = 5;
 task tasks[tasksNum];
 
 enum MIC_States {MIC_START, MIC_CHECK} state;
@@ -69,14 +59,21 @@ int TickFct_MIC(int state);
 enum LED_States {LED_START, LED_OFF, LED_ON};
 int TickFct_LED(int state);
 
+enum BUZZER_TOGGLE_STATES {BUZZER_TOGGLE_START, BUZZER_TOGGLE_WAIT, BUZZER_TOGGLE};
+int TickFct_BUZZER_TOGGLE(int state);
+
 enum BUZZER_STATES {BUZZER_START, BUZZER_OFF, BUZZER_ON};
 int TickFct_BUZZER(int state);
+
+enum NOKIA_STATES {NOKIA_START, NOKIA_DISPLAY};
+int TickFct_NOKIA(int state);
 
 void setup() {
   
   pinMode(micSensor_level, INPUT);
   pinMode(micSensor_intensity, INPUT);
   pinMode(ledPin, OUTPUT);
+  pinMode(button1, INPUT_PULLUP);
   pinMode(buzzer, OUTPUT);
   
   Serial.begin(9600);
@@ -92,89 +89,31 @@ void setup() {
   tasks[i].elapsedTime = 0;
   tasks[i].TickFct = &TickFct_LED;
   i++;
+  tasks[i].state = BUZZER_TOGGLE_START;
+  tasks[i].period = 50;
+  tasks[i].elapsedTime = 0;
+  tasks[i].TickFct = &TickFct_BUZZER_TOGGLE;
+  i++;
   tasks[i].state = BUZZER_START;
   tasks[i].period = 1000;
   tasks[i].elapsedTime = 0;
   tasks[i].TickFct = &TickFct_BUZZER;
   i++;
+  tasks[i].state = NOKIA_START;
+  tasks[i].period = 50;
+  tasks[i].elapsedTime = 0;
+  tasks[i].TickFct = &TickFct_NOKIA;
+  i++;
 
   //Initialize Display
   display.begin();
 
-  // you can change the contrast around to adapt the display for the best viewing!
+  //Set Contrast
   display.setContrast(57);
 
-  // Clear the buffer.
+  //Clear the buffer.
   display.clearDisplay();
 
-  // Display Text
-  display.setTextSize(1);
-  display.setTextColor(BLACK);
-  display.setCursor(0,0);
-  display.println("Hello world!");
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-
-  // Display Inverted Text
-  display.setTextColor(WHITE, BLACK); // 'inverted' text
-  display.setCursor(0,0);
-  display.println("Hello world!");
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // Scaling Font Size
-  display.setTextColor(BLACK);
-  display.setCursor(0,0);
-  display.setTextSize(2);
-  display.println("Hello!");
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // Display Numbers
-  display.setTextSize(1);
-  display.setCursor(0,0);
-  display.println(123456789);
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // Specifying Base For Numbers
-  display.setCursor(0,0);
-  display.print("0x"); display.print(0xFF, HEX); 
-  display.print("(HEX) = ");
-  display.print(0xFF, DEC);
-  display.println("(DEC)"); 
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // Display ASCII Characters
-  display.setCursor(0,0);
-  display.setTextSize(2);
-  display.write(3);
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // Text Rotation
-  while(1)
-  {
-  display.clearDisplay();
-  display.setRotation(rotatetext);  // rotate 90 degrees counter clockwise, can also use values of 2 and 3 to go further.
-  display.setTextSize(1);
-  display.setTextColor(BLACK);
-  display.setCursor(0,0);
-  display.println("Text Rotation");
-  display.display();
-  delay(1000);
-  display.clearDisplay();
-  rotatetext++;
-  }
-  
 }
 
 void loop() {
@@ -223,35 +162,11 @@ int TickFct_MIC(int state) {
         }
         
 
-        micLevel = analogRead(micSensor_level); //uncomment after below testing to revert
-        //micLevel = map(micLevel, 0, 700, 0, 10);
-        Serial.println(micLevel); //uncomment after below testing to revert
+        micLevel = analogRead(micSensor_level); 
+        micLevel = map(micLevel, 500, 538, 0, 10);
+        micLevel = abs(10 - micLevel);
+        Serial.println(micLevel); 
         
-        /*
-        //Testing with getting mic level values
-        int sum = 0;
-        int measurements = 128;
-        for (int i = 0; i < measurements; ++i) {
-          micLevel = analogRead(micSensor_level);
-          sum += micLevel;
-        }
-        micLevel = sum / measurements;
-        Serial.print("Sound Level: ");
-        Serial.println(micLevel - 38);
-        */
-
-        /*
-        int sensor_value = 0;
-        int threshold = 509;
-        int abs_value = 0;
-        int ledCount = 10;
-
-        sensor_value = analogRead(micSensor_level);
-        abs_value = abs(sensor_value - threshold);
-        int ledLevel = map(abs_value, 0, (1024 - threshold), 0, ledCount);
-        Serial.print("ledLevel: ");
-        Serial.println(ledLevel);
-        */
     break;
     
     
@@ -313,6 +228,55 @@ int TickFct_LED(int state) {
   return state;
 }
 
+int TickFct_BUZZER_TOGGLE(int state) {
+  switch(state){ // Transition States
+    case BUZZER_TOGGLE_START:
+        state = BUZZER_TOGGLE_WAIT;
+    break;
+
+    case BUZZER_TOGGLE_WAIT:
+        if (digitalRead(button1)) { //not pressed 
+          state = BUZZER_TOGGLE_WAIT;
+        }
+        else if (!digitalRead(button1)) {
+          buzzerEnabled = !buzzerEnabled;
+          state = BUZZER_TOGGLE;
+        }
+    break;
+
+    case BUZZER_TOGGLE:
+      if (!digitalRead(button1)) { //button remains pressed
+        state = BUZZER_TOGGLE;
+      }
+      else {
+        state = BUZZER_TOGGLE_WAIT;
+      }
+    break;
+    
+    default:
+      break;
+  }
+
+
+  switch(state){ // Action States
+    case BUZZER_TOGGLE_START:
+      
+    break;
+
+    case BUZZER_TOGGLE_WAIT:
+      
+    break;
+
+    case BUZZER_TOGGLE:
+      
+    break;
+    
+    default:
+      break;
+  }
+  return state;
+}
+
 int TickFct_BUZZER(int state) {
   switch(state){ // Transition States
     case BUZZER_START:
@@ -349,6 +313,50 @@ int TickFct_BUZZER(int state) {
 
     case BUZZER_ON:
       tone(buzzer, 200);
+    break;
+    
+    default:
+      break;
+  }
+  return state;
+}
+
+int TickFct_NOKIA(int state) {
+  switch(state){ // Transition States
+    case NOKIA_START:
+        state = NOKIA_DISPLAY;
+    break;
+
+    case NOKIA_DISPLAY:
+        state = NOKIA_DISPLAY;
+    break;
+    
+    default:
+      break;
+  }
+
+
+  switch(state){ // Action States
+    case NOKIA_START:
+      
+    break;
+
+    case NOKIA_DISPLAY:
+      display.clearDisplay();
+      display.setTextColor(BLACK);
+      display.setCursor(0, 0);
+      display.setTextSize(1);
+      display.println("Sound Level: ");
+      display.println(micLevel);
+      display.println("");
+      display.println("Intensity: ");
+      if (threshold_reached) {
+        display.println("HIGH");
+      }
+      else {
+        display.println("LOW");
+      }
+      display.display();
     break;
     
     default:
